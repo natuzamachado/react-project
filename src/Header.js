92bc87ca28565adc53a726d2e3decd955606a4e4
Natuza <natuza.machado@gmail.com>
2023-01-17 21:55:12 -0500

    react-proj

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <nav>
+                <img src="./reactjs-icon.png" alt="react logo"></img>
+                <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
+export default Header
